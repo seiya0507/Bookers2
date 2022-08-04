95c7dadba9991973a5aecdcd4cd926c9c8019ac9
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-08-04 09:46:15 +0000

    [Break] Bookers2 応用課題4

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root to: "homes#top"
   get '/home/about' => 'homes#about', as: 'about'
 
   resources :books do
-#   patch 'book/:id' => 'books#update', as: 'update_book'
 
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
-  resources :users
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 
 end
